f92e3b06a6a57c49471ab91d2441cd0e0f190281
Peter Fitch <peter.fitch.iii@gmail.com>
2020-04-07 11:00:59 -0700

    It's all running with the titles being generated automatically

--- build.py
+++ build.py
@@ -5,16 +5,19 @@ pages = [
         "filename" : "content/index.html",
         "output" : "docs/index.html",
         "title" : "About Me",
+        "nav_link" : "-About",
     },
     {
         "filename" : "content/experience.html",
         "output" : "docs/experience.html",
         "title" : "My Experience",
+        "nav_link" : "-Experience",
     },
     {
         "filename" : "content/contact.html",
         "output" : "docs/contact.html",
         "title" : "Contact Page",
+        "nav_link" : "-Contact"
     },
     
 ]
@@ -25,18 +28,7 @@ def template_read():
     template_read = open('./templates/base.html').read()
     print('template read ran')
     return template_read
-    
-#changing title 
-# def title_change(page):
-#     file_name = page['filename']
-#     output = page['output']
-#     title = page['title']
-#     template = template_read() #template (top/bottom) read and put into template var
-#     index_content = open(file_name).read() #opens the content files: about/experience/contact and puts into var
-#     content = template.replace("{{title}}", index_content) #does the actual replacement for each of the above on each loop 
-#     writing_output(output,content)#writes to output file destination
-#     print('apply title ran')
-          
+             
 
 
 #uses page variable to loop through when called to apply (missing) content to template
@@ -44,9 +36,11 @@ def apply_template(page):
     file_name = page['filename']
     output = page['output']
     title = page['title']
+    nav_link = page['nav_link']
     template = template_read() #template (top/bottom) read and put into template var
     index_content = open(file_name).read() #opens the content files: about/experience/contact and puts into var
     content = template.replace("{{content}}", index_content) #does the actual replacement for each of the above on each loop 
+    content = content.replace("{{title}}", title)
     content = content.replace("{{title}}", title)
     writing_output(output,content)#writes to output file destination
     print('apply template ran')
