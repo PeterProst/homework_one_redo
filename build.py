7b3eeb3403b40f7363f8b99becf68eb45c6bdac0
Peter Fitch <peter.fitch.iii@gmail.com>
2020-04-10 08:31:11 -0700

    got everything working including the new template reader

--- build.py
+++ build.py
@@ -1,42 +1,47 @@
 
-#list of pages that are accessed via for loop
-pages = [
-    {
-        "filename" : "content/index.html",
-        "output" : "docs/index.html",
-        "title" : "About Me",
-    },
-    {
-        "filename" : "content/experience.html",
-        "output" : "docs/experience.html",
-        "title" : "My Experience",
-    },
-    {
-        "filename" : "content/contact.html",
-        "output" : "docs/contact.html",
-        "title" : "Contact Page",
-    },
-    
-]
+from jinja2 import Template
+import glob, os
+
+pages = []
+
+def auto_generate_content():
+    all_html_files = glob.glob("content/*.html")
+    for html_file in all_html_files:
+        file_name = os.path.basename(html_file)
+        output = "docs/" + file_name
+        name_only, extension = os.path.splitext(file_name)
+        pages.append({
+            "filename" : html_file,
+            "title" : name_only,
+            "output" : output,
+        })
+        
+ 
+
+auto_generate_content()
 
 
-#this opens and reads the base html
-def template_read():
-    template_read = open('./templates/base.html').read()
+
+# this opens and reads the base html
+def template_html():
+    template_html = open('templates/base.html').read()
     print('template read ran')
-    return template_read
+    return template_html
              
 
 
 #uses page variable to loop through when called to apply (missing) content to template
 def apply_template(page):
-    file_name = page['filename']
-    output = page['output']
-    title = page['title']
-    template = template_read() #template (top/bottom) read and put into template var
-    index_content = open(file_name).read() #opens the content files: about/experience/contact and puts into var
-    content = template.replace("{{content}}", index_content) #does the actual replacement for each of the above on each loop 
-    content = content.replace("{{title}}", title)
+    file_name = page["filename"]
+    output = page["output"]
+    page_title = page["title"]
+    template = Template(template_html()) #template (top/bottom) read and put into template var
+    index_html = open(file_name).read() #opens the content files: about/experience/contact and puts into var
+    content = template.render(
+        title = page_title,
+        content = index_html,
+    ) #does the actual replacement for each of the above on each loop 
+    # content = content.replace("{{title}}", page_title)#updates content to include the dynamic title 
     writing_output(output,content)#writes to output file destination
     print('apply template ran')
 
